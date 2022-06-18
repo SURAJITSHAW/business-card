cb3572d200bb7ef43e75d991994e3884efd94130
Surajit <piklu3043@gmail.com>
2022-06-18 20:12:15 +0530

    Modification on Card UI

--- src/App.js
+++ src/App.js
@@ -10,11 +10,20 @@ function App() {
           <h3 className="card-title">Surajit Shaw</h3>
           <h6 className="mt-2 card-subtitle text-warning"><small>Front-end Developer</small></h6>
           <h6 className="card-subtitle text-secondary my-1"><small>surajit-portfolio.com</small></h6>
-          <a href="#" className="btn btn-light mt-4" style={{width: "100%"}}>Email</a>
+          <a href="#" className="btn btn-light mt-4 " style={{width: "100%"}}>Email</a>l
         </div>
         <div className="card-body">
         <h3>About</h3>
         <p className="lead">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+        </div>
+        <div className="card-footer">
+          <div className="row justify-content-around">
+            <div className="col-2">Hello</div>
+            <div className="col-2">Hello</div>
+            <div className="col-2">Hello</div>
+            <div className="col-2">Hello</div>
+            <div className="col-2">Hello</div>
+          </div>
         </div>
       </div>
     </div>
